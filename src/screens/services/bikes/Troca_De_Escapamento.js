6628ed8d7aede7277b7edcafa97aaaac63a79c77
José Lucas Matos dos Santos <jose.lucas@rodobank.com.br>
2022-11-18 09:33:52 -0300

    correção DataMinima datepicker Bikes

--- src/screens/services/bikes/Troca_De_Escapamento.js
+++ src/screens/services/bikes/Troca_De_Escapamento.js
@@ -73,6 +73,7 @@ export default function Troca_De_Escapamento({ navigation }) {
                     locale='pt-BR'
                     minuteInterval={30}
                     themeVariant="light"
+                    minimumDate={new Date()}
                 />
 
                 <Text style={styles.infoEndTimeService} >
--- src/screens/services/bikes/Troca_Disco_De_Freio.js
+++ src/screens/services/bikes/Troca_Disco_De_Freio.js
@@ -73,6 +73,7 @@ export default function Troca_Disco_De_Freio({ navigation }) {
                     locale='pt-BR'
                     minuteInterval={30}
                     themeVariant="light"
+                    minimumDate={new Date()}
                 />
 
                 <Text style={styles.infoEndTimeService} >
